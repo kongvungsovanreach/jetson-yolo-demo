3568e0ae84008d436a74ff9dc51f3b61c8a0fac0
KONG Vungsovanreach <54223869+kongvungsovanreach@users.noreply.github.com>
2024-04-05 14:17:48 +0900

    Update run-yolo.sh

--- run-yolo.sh
+++ run-yolo.sh
@@ -3,4 +3,4 @@
 cd /home/nvidia/Desktop/jetson-demo && export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/gstreamer-1.0/libgstnvvidconv.so && /usr/bin/python3 run_yolo_gpio.py
 
 #for cronjob
-#@reboot DISPLAY=:0 /home/nvidia/Desktop/jetson-demo/run-yolo.sh >> /home/nvidia/Desktop/jetson-demo/yolo.log 2>&1
+#@reboot /usr/bin/sleep 30 && DISPLAY=:0 /home/nvidia/Desktop/jetson-demo/run-yolo.sh >> /home/nvidia/Desktop/jetson-demo/yolo.log 2>&1
