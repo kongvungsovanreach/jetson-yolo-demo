fa77ce43067d82b98e1d7b54594a0e2dea354167
Kong Vungsovanreach <vungsovanreachkong@gmail.com>
2024-04-08 16:25:42 +0900

    improve motion detection

--- modules/capture_util.py
+++ modules/capture_util.py
@@ -2,6 +2,7 @@
 import os, cv2, random, uuid
 from datetime import datetime
 from pathlib import Path
+import math
 
 DATA_ROOT = 'frames'
 
@@ -25,3 +26,32 @@ def random_save(image, total_folder, folder_prefix='folder'):
     cv2.imwrite(image_path, image)
 
     return selected_folder, image_path
+
+
+#get scale font size for different plotting frame size
+def get_scaled_font(fw, fh, size_ratio=2e-3, thickness_scale=1e-3):
+    font_scale = min(fw, fh) * size_ratio
+    thickness_scale = math.ceil(min(fw, fh) * thickness_scale)
+    return font_scale, thickness_scale
+
+#store configuration/variable for inner/outer function use
+class Config():
+    def __init__(self):
+        #dictionary to store dynamic variables
+        self._dynamic_variables = {}
+
+    def __getattr__(self, name):
+        #this method is called when an attribute is not found
+        if name in self._dynamic_variables:
+            return self._dynamic_variables[name]
+        else:
+            raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
+
+    def __setattr__(self, name, value):
+        #this method is called when an attribute is set
+        if name == '_dynamic_variables':
+            #allow setting the _dynamic_variables attribute directly
+            super().__setattr__(name, value)
+        else:
+            #set a dynamic variable
+            self._dynamic_variables[name] = value
